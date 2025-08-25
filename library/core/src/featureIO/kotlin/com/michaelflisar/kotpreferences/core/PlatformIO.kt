6f50277c6211057d81162dfef8fb458a77f23de7
MFlisar <mflisar.development@gmail.com>
2025-08-25 12:11:54 +0200

    ...

--- library/core/src/featureIO/kotlin/com/michaelflisar/kotpreferences/core/PlatformIO.kt
+++ library/core/src/featureIO/kotlin/com/michaelflisar/kotpreferences/core/PlatformIO.kt
@@ -1,6 +1,7 @@
 package com.michaelflisar.kotpreferences.core
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlin.coroutines.CoroutineContext
 
 actual val StorageContext: CoroutineContext = Dispatchers.IO
